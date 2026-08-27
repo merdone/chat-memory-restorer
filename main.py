3c8ef45c0d800e96d6cffacc7f0384fe81e7dea4
merdone <maksnevino@gmail.com>
2026-08-27 18:31:39 +0300

    updated structure of processing message function, added function for generation random names for file, started function for processing downloading

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import telethon
 from dotenv import load_dotenv
 from os import getenv
 from telethon import TelegramClient
+import random
+import pathlib
+import string
 
 load_dotenv()
 
@@ -15,32 +18,60 @@ client = TelegramClient('anon', api_id, api_hash)
 # client.start(phone=phone_number, password=account_password)
 
 async def process_message(message: telethon.types.Message) -> dict | None:
+    chat_id = message.peer_id.user_id
+
     message_id = message.id
     message_send_date = message.date
 
     media_date = None
-    media_type = "Unknown"
-    message_media = message.media
-    if isinstance(message_media, telethon.types.MessageMediaDocument):
-        if message_media.voice:
-            media_type = "voice"
-        elif message_media.round:
-            media_type = "round"
-        elif message.media.video:
-            media_type = "video"
-        else:
-            media_type = "document"
-        document = message_media.document
-        media_date = document.date
-    elif isinstance(message_media, telethon.types.MessageMediaPhoto):
-        media_type = "photo"
-        photo = message_media.photo
-        media_date = photo.date
+    downloadable = False
+    media_type = "unknown"
 
-    return {"message_id": message_id,
+    if message.media:
+        message_media = message.media
+        if isinstance(message_media, telethon.types.MessageMediaDocument):
+            if message_media.voice:
+                media_type = "voice"
+            elif message_media.round:
+                media_type = "round"
+            elif message.media.video:
+                media_type = "video"
+            else:
+                media_type = "document"
+            document = message_media.document
+            media_date = document.date
+        elif isinstance(message_media, telethon.types.MessageMediaPhoto):
+            media_type = "photo"
+            photo = message_media.photo
+            media_date = photo.date
+        downloadable = True
+    else:
+        media_type = "text"
+
+    return {"chat_id": chat_id,
+            "message_id": message_id,
             "media_type": media_type,
             "message_send_date": message_send_date,
-            "media_date": media_date}
+            "media_date": media_date,
+            "downloadable": downloadable}
+
+async def generate_name(date) -> str:
+    random_part = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=10))
+    return date.strftime('%d_%m_%Y_') + random_part
+
+
+async def process_downloading(message, processed_dict):
+    if processed_dict.get("downloadable"):
+        chat_id = str(processed_dict.get("chat_id"))
+        # process metadata changing
+        if processed_dict.get("media_date"):
+            # set this to metadate
+            filename = await generate_name(processed_dict.get("media_date"))
+
+            path = pathlib.Path().resolve() / chat_id /filename
+
+            await message.download_media(file=path)
+
 
 
 async def main():
@@ -48,16 +79,13 @@ async def main():
     read_message = await client.get_messages(911873858, limit=None, reverse=True)
 
     # print(read_message[-1].stringify())
-
-    if (read_message[-1].media):
-        print(await process_message(read_message[-1]))
-    else:
-        print("it's not a photo/document")
+    a = await process_message(read_message[-1])
+    print(a)
 
     print("")
-
-    path = await read_message[-1].download_media()
-    print('File saved to', path)  # printed after download is done
+    await process_downloading(read_message[-1], a)
+    # path = await read_message[-1].download_media()
+    # print('File saved to', path)  # printed after download is done
 
     # async for message in client.iter_messages(911873858, reverse=True):
     #     print(message.id, message.text)
