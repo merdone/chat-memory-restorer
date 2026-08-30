d8a0a9688d6acf666acbc93ad529a0fd4a68a3e9
merdone <maksnevino@gmail.com>
2026-08-31 01:56:47 +0300

    replaced configurating from main file to separated module

---
+++ config.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+from os import getenv
+
+from models import Config
+
+load_dotenv()
+
+api_id = int(getenv("api_id"))
+api_hash = getenv("api_hash")
+phone_number = getenv("phone_number")
+account_password = getenv("account_password")
+
+config = Config(api_id, api_hash, phone_number, account_password)
